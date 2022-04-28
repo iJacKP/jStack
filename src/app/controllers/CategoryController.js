7ef73c48d12253a3cab192a21a306b49ef782e28
Jack <jackson_d2@outlook.com>
2022-04-28 10:54:25 -0300

    Category CRUD now is completed

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -5,9 +5,9 @@ class CategoryController {
     async index(req,res) {
 
 
-        // const { orderBy } = req.query
+        const { orderBy } = req.query
 
-        // const categories = await CategoriesRepository.findAll(orderBy)
+        const categories = await CategoriesRepository.findAll(orderBy)
 
         res.json(categories)
 
@@ -25,6 +25,43 @@ class CategoryController {
 
         return res.json(category)
 
+    }
+
+    async show(req,res){
+
+        const { id } = req.params
+
+        const categoryExists = await  CategoriesRepository.findById(id)
+
+        if(!categoryExists) {
+            res.status(404).json({error : 'Category not found'})
+        }
+
+        res.json(categoryExists)
+
+    }
+
+    async update(req,res){
+        const { name } = req.body
+        const { id } = req.params
+
+
+        if(!name){
+            res.status(400).json({ error : "Name is required "})
+        }
+
+        const categoryUpdated = await CategoriesRepository.update(id , { name })
+
+        res.json(categoryUpdated)
+
+    }
+
+    async delete(req,res){
+        const { id } = req.params
+
+        await CategoriesRepository.delete(id)
+
+        res.sendStatus(204)
     }
 
 }
